0b079b45a04105fe3da156ed1c8978f6b0597544
Fredrik <181095176+HuckerDuck@users.noreply.github.com>
2025-05-08 12:47:34 +0200

    It's working as it's supposed to, am glad for the error fixing

--- lektion-4-react/src/components/CustomButton.test.tsx
+++ lektion-4-react/src/components/CustomButton.test.tsx
@@ -2,40 +2,28 @@ import { render, fireEvent } from '@testing-library/react'
 import { CustomButton, ButtonState } from './CustomButton'
 import '@testing-library/jest-dom'
 
-describe('Easy Examples', () => {
-  const buttonText = 'signup'
 
-  test('starts idle, goes to hover on mouse enter, and back to idle on mouse leave', () => {
-    const { getByText } = render(<CustomButton buttonText={buttonText} />)
-    const btn = getByText(buttonText)
+describe(CustomButton, () => {
+    test("Has correct class before/after onClick/hover", () => {
+    // Init
+    const buttonName: string = "submit"
+    const { getByText } = render(<CustomButton buttonText={buttonName} />)
+    const button = getByText(buttonName)
+    // Idle
+    expect(button).toHaveClass(ButtonState.idle)
+    // Hovered
+    fireEvent.mouseEnter(button)
+    expect(button).toHaveClass(ButtonState.hover)
+    // Clicked
+    fireEvent.click(button)
+    expect(button).toHaveClass(ButtonState.clicked)
 
-    // initial
-    expect(btn).toHaveClass(ButtonState.idle)
+    
 
-    // hover
-    fireEvent.mouseEnter(btn)
-    expect(btn).toHaveClass(ButtonState.hover)
-
-    // leave
-    fireEvent.mouseLeave(btn)
-    expect(btn).toHaveClass(ButtonState.idle)
     })
-  })
 
-  const buttonText = 'signup'
-
-  test('goes to clicked on click', () => {
-    const { getByText } = render(<CustomButton buttonText={buttonText} />)
-    const btn = getByText(buttonText)
-
-    // initial
-    expect(btn).toHaveClass(ButtonState.idle)
-
-    // click
-    fireEvent.click(btn)
-    expect(btn).toHaveClass(ButtonState.clicked)
-  })
-
-  test("this test should work for my GitHub action Training", () => {
-    expect(2+2).toBe(4)
+    test("this test should work for my GitHub action Training", () => {
+        expect(2+2).toBe(4)
+    })
 })
+
